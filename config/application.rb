cca7bc7445a83f9fd214f8b0a4ee856bde933adb
Victor Pudeyev <piousbox@gmail.com>
2012-07-21 09:05:19 -0700

    restructuring git submodules

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
 # require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
@@ -18,14 +17,9 @@ end
 module ViewsReports
   class Application < Rails::Application
     
-    config.assets.paths << Rails.root.join("app", "data")
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -37,9 +31,7 @@ module ViewsReports
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    
     config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
@@ -54,6 +46,26 @@ module ViewsReports
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.autoload_paths += %W( #{config.root}/app/no_models )
+    
+    
+    
+    #
+    # ish-lib
+    #
+    config.autoload_paths += %W(
+      #{config.root}/lib/ish-lib/controllers
+      #{config.root}/lib/ish-lib/models
+    )
+    config.i18n.load_path += Dir[Rails.root.join('lib', 'ish-lib', 'locales', '*.{rb,yml}').to_s]
+    
+    config.autoload_paths += %W(
+      #{config.root}/lib/ish-lib/controllers
+      #{config.root}/lib/ish-lib/models
+    )
+    config.assets.paths << Rails.root.join("lib", "ish-lib", 'assets', 'flash' )
+    config.assets.paths << Rails.root.join("lib", "ish-lib", 'assets', 'images' )
+    config.assets.paths << Rails.root.join("lib", "ish-lib", 'assets', 'javascripts' )
+    config.assets.paths << Rails.root.join("lib", "ish-lib", 'assets', 'stylesheets' )
+    config.assets.paths << Rails.root.join("lib", "ish-lib", 'data' )
   end
 end
